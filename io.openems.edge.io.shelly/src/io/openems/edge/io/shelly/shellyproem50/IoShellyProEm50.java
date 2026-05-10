18bd49da8330bad299066be231feb1bc221262c5
Simon <simonfrei@gmx.ch>
2026-05-10 16:56:56 +0200

    feat: add Shelly Pro EM-50 interface

--- io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyproem50/IoShellyProEm50.java
+++ io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyproem50/IoShellyProEm50.java
@@ -1,4 +1,4 @@
-package io.openems.edge.io.shelly.shellypluspmmini;
+package io.openems.edge.io.shelly.shellyproem50;
 
 import org.osgi.service.event.EventHandler;
 
@@ -8,14 +8,14 @@ import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.meter.api.ElectricityMeter;
 import io.openems.edge.meter.api.SinglePhaseMeter;
 
-public interface IoShellyPlusPmMini extends SinglePhaseMeter, ElectricityMeter, OpenemsComponent, EventHandler {
+public interface IoShellyProEm50 extends SinglePhaseMeter, ElectricityMeter, OpenemsComponent, EventHandler {
 
 	public static enum ChannelId implements io.openems.edge.common.channel.ChannelId {
 		/**
 		 * Indicates whether the Shelly needs a restart.
 		 *
 		 * <ul>
-		 * <li>Interface: ShellyPlusPMMini
+		 * <li>Interface: ShellyProEM50
 		 * <li>Type: State
 		 * <li>Level: INFO
 		 * </ul>
@@ -26,7 +26,7 @@ public interface IoShellyPlusPmMini extends SinglePhaseMeter, ElectricityMeter, 
 		 * Slave Communication Failed Fault.
 		 *
 		 * <ul>
-		 * <li>Interface: ShellyPlusPMMini
+		 * <li>Interface: ShellyProEM50
 		 * <li>Type: State
 		 * <li>Level: FAULT
 		 * </ul>
