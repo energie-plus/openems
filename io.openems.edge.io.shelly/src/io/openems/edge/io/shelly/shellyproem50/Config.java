c9386a9eb18934dbb4d8c2990b9d40af644cfe66
Simon <simonfrei@gmx.ch>
2026-05-10 16:55:15 +0200

    feat: add Shelly Pro EM-50 config

--- io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyproem50/Config.java
+++ io.openems.edge.io.shelly/src/io/openems/edge/io/shelly/shellyproem50/Config.java
@@ -1,4 +1,4 @@
-package io.openems.edge.io.shelly.shellypluspmmini;
+package io.openems.edge.io.shelly.shellyproem50;
 
 import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
@@ -7,12 +7,12 @@ import io.openems.common.types.MeterType;
 import io.openems.edge.common.type.Phase.SinglePhase;
 
 @ObjectClassDefinition(//
-		name = "IO Shelly Plus PM Mini", //
-		description = "Implements the Shelly Plus PM Mini 2nd and 3rd Gen")
+		name = "IO Shelly Pro EM-50", //
+		description = "Implements the Shelly Pro EM-50")
 @interface Config {
 
 	@AttributeDefinition(name = "Component-ID", description = "Unique ID of this Component")
-	String id() default "ioShellyPmMini0";
+	String id() default "meter0";
 
 	@AttributeDefinition(name = "Alias", description = "Human-readable name of this Component; defaults to Component-ID")
 	String alias() default "";
@@ -20,17 +20,20 @@ import io.openems.edge.common.type.Phase.SinglePhase;
 	@AttributeDefinition(name = "Is enabled?", description = "Is this Component enabled?")
 	boolean enabled() default true;
 
-	@AttributeDefinition(name = "Phase", description = "Which Phase is this Shelly Plug connected to?")
+	@AttributeDefinition(name = "Phase", description = "Which phase is measured by this Shelly Pro EM-50 channel?")
 	SinglePhase phase() default SinglePhase.L1;
 
 	@AttributeDefinition(name = "IP-Address", description = "The IP address of the Shelly device.")
 	String ip();
 
-	@AttributeDefinition(name = "Meter-Type", description = "What is measured by this Meter?")
-	MeterType type() default MeterType.CONSUMPTION_METERED;
+	@AttributeDefinition(name = "Channel", description = "Shelly EM1 channel id. Usually 0 or 1.")
+	int channel() default 0;
 
-	@AttributeDefinition(name = "Invert Power", description = "Inverts all Power values, inverts current values, swaps production and consumptioon energy, i.e. Power is multiplied with -1.")
+	@AttributeDefinition(name = "Meter-Type", description = "What is measured by this Meter?")
+	MeterType type() default MeterType.GRID;
+
+	@AttributeDefinition(name = "Invert Power", description = "Inverts all power values, inverts current values, swaps production and consumption energy.")
 	boolean invert() default false;
 
-	String webconsole_configurationFactory_nameHint() default "IO Shelly Plus PM Mini [{id}]";
+	String webconsole_configurationFactory_nameHint() default "IO Shelly Pro EM-50 [{id}]";
 }
