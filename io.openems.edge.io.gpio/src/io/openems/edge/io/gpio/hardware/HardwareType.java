17d4cec79aba521c2bc273392c85e3c067888470
Simon <simonfrei@gmx.ch>
2026-05-17 23:51:11 +0200

    feat: add Modberry X500 M41601WB MAX gpio platform

--- io.openems.edge.io.gpio/src/io/openems/edge/io/gpio/hardware/HardwareType.java
+++ io.openems.edge.io.gpio/src/io/openems/edge/io/gpio/hardware/HardwareType.java
@@ -3,6 +3,7 @@ package io.openems.edge.io.gpio.hardware;
 import io.openems.edge.io.gpio.linuxfs.HardwareFactory;
 
 public enum HardwareType {
+	MODBERRY_X500_M41601WB_MAX, //
 	MODBERRY_X500_M40804_MAX, //
 	MODBERRY_X500_M40804_WB, //
 	MODBERRY_X500_M4S, //
@@ -18,6 +19,7 @@ public enum HardwareType {
 	public HardwarePlatform createInstance(String gpioPath) {
 		var factory = new HardwareFactory(gpioPath);
 		return switch (this) {
+		case MODBERRY_X500_M41601WB_MAX -> new ModberryX500M41601WbMax(factory);
 		case MODBERRY_X500_M40804_MAX -> new ModberryX500M40804Max(factory);
 		case MODBERRY_X500_M40804_WB -> new ModberryX500M40804Wb(factory);
 		case MODBERRY_X500_M4S, MODBERRY_X500_M4S_F, MODBERRY_X500_M3 -> new ModberryX500M4S(factory);
